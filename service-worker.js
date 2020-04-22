0a5bc9bb166f0d9bc90f44c1095a3d4793b23494
romarios1987 <romios047@gmail.com>
2020-04-22 15:39:28 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.421beebb3ffb134bcb3bbbe3c2b57c37.js"
 );
 
 self.addEventListener('message', (event) => {
